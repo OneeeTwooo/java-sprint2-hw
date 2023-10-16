d1175d6f87206e09caa67405d96865ff34f7f3c4
Artsiom Vasileuski <artsiom.vasileuski@gmail.com>
2023-10-16 22:36:18 +0300

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Поехали!
+        FileReader fr = new FileReader();
+        ArrayList<String> strings = fr.readFileContents("y.2021.csv");
+        for (String st: strings){
+            System.out.println(st);
+        }
     }
 }
 
